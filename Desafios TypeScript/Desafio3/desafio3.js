a4ac17a525ed4217593b52046b6f6b003215d179
Michael <michaellima.contato@gmail.com>
2022-07-01 16:37:44 -0300

    desafio3 completo

---
+++ Desafios TypeScript/Desafio3/desafio3.js
@@ -0,0 +1,29 @@
+var botaoAtualizar = document.getElementById('atualizar-saldo');
+var botaoLimpar = document.getElementById('limpar-saldo');
+var soma = document.getElementById('soma');
+var campoSaldo = document.getElementById('campo-saldo');
+var saldoTotal = 0;
+function adicionarValor(soma) {
+    if (campoSaldo) {
+        saldoTotal += soma;
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', function () {
+        adicionarValor(Number(soma.value));
+    });
+}
+function limparCampoSoma() {
+    soma.value = "";
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
---
+++ Desafios TypeScript/Desafio3/desafio3.ts
@@ -0,0 +1,34 @@
+
+let botaoAtualizar = document.getElementById('atualizar-saldo')!;
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo')!;
+
+let saldoTotal = 0
+
+
+function adicionarValor(soma: number) {
+    saldoTotal += soma
+    campoSaldo.innerHTML = saldoTotal.toString();
+    limparCampoSoma();
+
+}
+
+
+botaoAtualizar.addEventListener('click', () => {
+    adicionarValor(Number(soma.value));
+});
+
+function limparCampoSoma() {
+    soma.value = "";
+}
+
+function limparSaldo() {    
+    saldoTotal = 0;
+    campoSaldo.innerHTML = saldoTotal.toString();
+    
+}
+
+botaoLimpar.addEventListener('click', () => {
+    limparSaldo();
+});
